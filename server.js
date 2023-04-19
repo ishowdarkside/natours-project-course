e9cd4d9aeea267cbe5cbee9962528b184901105e
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-04-19 02:22:59 +0200

    end

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const app = require('./app');
 const port = +process.env.PORT || 3000;
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE__PASSWORD
-);
+const DB = process.env.DATABASE;
 
 mongoose.connect(DB).then((con) => {
   console.log('DB Connection successful');
